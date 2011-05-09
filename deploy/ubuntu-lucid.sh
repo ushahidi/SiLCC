78e1dd3849de9dfa9f576628d72e1148edfc635d
Charl van Niekerk <charlvn@charlvn.com>
2011-05-09 14:25:00 +0200

    Added Ubuntu Lucid deployment script.

---
+++ deploy/ubuntu-lucid.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+aptitude update
+aptitude safe-upgrade -y
+aptitude install -y git-core unzip python-virtualenv python-setuptools python-dev python-pastescript python-numpy mysql-server-5.1 libmysqlclient-dev
+easy_install mysql-python
+easy_install pyyaml
+easy_install SQLAlchemy==0.6.0
+easy_install sqlalchemy-migrate==0.5.4
+git clone https://github.com/ushahidi/SiLCC.git
+cd SiLCC
+python setup.py develop
+cd ..
+wget http://nltk.googlecode.com/files/nltk-2.0b8.zip
+unzip nltk-2.0b8.zip
+cd nltk-2.0b8
+python setup.py install
+python -c "import nltk;nltk.download('maxent_treebank_pos_tagger')"
+cd ../SiLCC
+echo "create database silcc default charset utf8;grant all on silcc.* to silcc@localhost identified by 'password';" | mysql -u root -p
+python db_repository/manage.py version_control mysql://silcc:password@localhost:3306/silcc
+migrate manage manage.py --repository=db_repository --url=mysql://silcc:password@localhost:3306/silcc
+python manage.py upgrade
+paster serve --daemon development.ini
