4fa542a176fae160c1523b7a21aa1b9e3d60f945
Charl van Niekerk <charlvn@charlvn.com>
2011-05-10 13:27:10 +0200

    Use prod config.

--- deploy/ubuntu-lucid-production.sh
+++ deploy/ubuntu-lucid-production.sh
@@ -24,4 +24,4 @@ python db_repository/manage.py version_control mysql://silcc:password@localhost:
 migrate manage manage.py --repository=db_repository --url=mysql://silcc:password@localhost:3306/silcc
 python manage.py upgrade
 echo "insert into apikey set keystr = 'AAAABBBB', valid_domains = '*';" | mysql -u root -p silcc
-paster serve --daemon development.ini
+paster serve --daemon prod.ini
